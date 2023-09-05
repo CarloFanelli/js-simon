2ba4f41891c81800037bfa9109617377383ed3fe
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-05 15:27:09 +0200

    move all the code from function 'convertMsInS inside setInterval function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina! */
 
 
-// .getTime transform all in Ms
+/* // .getTime transform all in Ms
 const timeNowMS = new Date().getTime();
 
 // finish date
@@ -19,23 +19,71 @@ console.log(remainingTime);
 
 function convertMsInTime(remainingTime) {
 
-    const secondConverted = Math.floor(remainingTime / 1000);
+    if (remainingTime < 1) {
 
-    const remainingS = secondConverted % 60;
+        document.getElementById('time').innerHTML = 'tempo scaduto';
 
-    const minutesConverted = Math.floor(secondConverted / 60);
+    } else {
 
-    const remainingM = minutesConverted % 60;
+        const secondConverted = Math.floor(remainingTime / 1000);
 
-    const hourConverted = Math.floor(minutesConverted / 60);
+        const remainingS = secondConverted % 60;
 
-    const remainingH = hourConverted % 24;
+        const minutesConverted = Math.floor(secondConverted / 60);
 
-    const daysConverted = Math.floor(hourConverted / 24);
+        const remainingM = minutesConverted % 60;
 
-    document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
-    
-}
+        const hourConverted = Math.floor(minutesConverted / 60);
 
-convertMsInTime(remainingTime);
+        const remainingH = hourConverted % 24;
+
+        const daysConverted = Math.floor(hourConverted / 24);
+
+        document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
+
+    }
+} */
+
+setInterval(function () {
+
+    const timeNowMS = new Date().getTime();
+
+    // finish date
+    const expirationTime = new Date('2023, 9 , 6, 9:30:00').getTime();
+
+    //console.log(timeNowMS);
+
+    //console.log(expirationTime);
+
+    const remainingTime = expirationTime - timeNowMS;
+
+    //console.log(remainingTime);
+
+    if (remainingTime < 1) {
+
+        document.getElementById('time').innerHTML = 'tempo scaduto';
+
+    } else {
+
+        const secondConverted = Math.floor(remainingTime / 1000);
+
+        const remainingS = secondConverted % 60;
+
+        const minutesConverted = Math.floor(secondConverted / 60);
+
+        const remainingM = minutesConverted % 60;
+
+        const hourConverted = Math.floor(minutesConverted / 60);
+
+        const remainingH = hourConverted % 24;
+
+        const daysConverted = Math.floor(hourConverted / 24);
+
+        document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
+
+    }
+
+
+
+}, 1);
 
