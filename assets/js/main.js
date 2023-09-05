c0faf9cf55097f07bf06bdca92662ce3d060e63c
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-05 14:43:25 +0200

    get Ms when page is load and remaining ms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 /* Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina! */
 
+
+// .getTime transform all in Ms
 const timeNowMS = new Date().getTime();
 
+// finish date
 const expirationTime = new Date('2023, 9 , 5, 9:30:00').getTime();
 
 console.log(timeNowMS);
