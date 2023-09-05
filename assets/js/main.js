ff599a4334a32fdce5e56932a5578e1556d0755e
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-05 15:42:12 +0200

    remove from anonimous function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,26 @@
 /* Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina! */
 
-
-/* // .getTime transform all in Ms
+/* 
+// .getTime transform all in Ms
 const timeNowMS = new Date().getTime();
 
 // finish date
 const expirationTime = new Date('2023, 9 , 6, 9:30:00').getTime();
 
-console.log(timeNowMS);
+//console.log(timeNowMS);
 
-console.log(expirationTime);
+//console.log(expirationTime);
 
 const remainingTime = expirationTime - timeNowMS;
 
-console.log(remainingTime);
+//console.log(remainingTime);
+ */
 
 
-function convertMsInTime(remainingTime) {
+function convertMsInTime() {
 
-    if (remainingTime < 1) {
-
-        document.getElementById('time').innerHTML = 'tempo scaduto';
-
-    } else {
-
-        const secondConverted = Math.floor(remainingTime / 1000);
-
-        const remainingS = secondConverted % 60;
-
-        const minutesConverted = Math.floor(secondConverted / 60);
-
-        const remainingM = minutesConverted % 60;
-
-        const hourConverted = Math.floor(minutesConverted / 60);
-
-        const remainingH = hourConverted % 24;
-
-        const daysConverted = Math.floor(hourConverted / 24);
-
-        document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
-
-    }
-} */
-const expiringTime = setInterval(function () {
-
+    // .getTime transform all in Ms
     const timeNowMS = new Date().getTime();
 
     // finish date
@@ -62,8 +38,6 @@ const expiringTime = setInterval(function () {
 
         document.getElementById('time').innerHTML = 'tempo scaduto';
 
-        clearInterval(expiringTime);
-
     } else {
 
         const secondConverted = Math.floor(remainingTime / 1000);
@@ -83,8 +57,8 @@ const expiringTime = setInterval(function () {
         document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
 
     }
+}
 
+const expiringTime = setInterval(convertMsInTime, 1);
 
-
-}, 100);
-
+console.log(expiringTime);
