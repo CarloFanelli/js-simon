4761f81dd0d32c3f25c25da5e098c4754ff34550
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-05 15:20:56 +0200

    print in page remaining time when page charge

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,36 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 const timeNowMS = new Date().getTime();
 
 // finish date
-const expirationTime = new Date('2023, 9 , 5, 9:30:00').getTime();
+const expirationTime = new Date('2023, 9 , 6, 9:30:00').getTime();
 
 console.log(timeNowMS);
 
 console.log(expirationTime);
 
-const remainingTime = timeNowMS - expirationTime;
+const remainingTime = expirationTime - timeNowMS;
 
 console.log(remainingTime);
+
+
+function convertMsInTime(remainingTime) {
+
+    const secondConverted = Math.floor(remainingTime / 1000);
+
+    const remainingS = secondConverted % 60;
+
+    const minutesConverted = Math.floor(secondConverted / 60);
+
+    const remainingM = minutesConverted % 60;
+
+    const hourConverted = Math.floor(minutesConverted / 60);
+
+    const remainingH = hourConverted % 24;
+
+    const daysConverted = Math.floor(hourConverted / 24);
+
+    document.getElementById('time').innerHTML = `${daysConverted} giorni, ${remainingH} ore, ${remainingM} minuti, ${remainingS} secondi`;
+    
+}
+
+convertMsInTime(remainingTime);
+
